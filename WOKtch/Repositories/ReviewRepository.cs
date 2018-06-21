3206904eb309a083e586326213d8642cfd269556
Naufal Faris R <nfr2208@gmail.com>
2018-06-21 16:08:16 +0700

    Merge update delete review

--- WOKtch/Repositories/ReviewRepository.cs
+++ WOKtch/Repositories/ReviewRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using WOKtch.Factories;
 using WOKtch.Models;
 using WOKtch.Utilities;
 
@@ -18,26 +19,43 @@ namespace WOKtch.Repositories
             return dt;
         }
 
+        public static Review GetById(int ReviewId)
+        {
+            string query = string.Format("SELECT * FROM [Review] WHERE (ReviewId) = {0}", ReviewId);
+
+            DataTable dt = DatabaseConnection.ExecuteQuery(query);
+            Review u = null;
+            foreach (DataRow x in dt.Rows)
+            {
+                u = ReviewFactory.Create(Int32.Parse(x["ReviewRating"].ToString()),
+                                            x["ReviewText"].ToString(),
+                                            Int32.Parse(x["ProductId"].ToString()),
+                                            Int32.Parse(x["UserId"].ToString()));
+            }
+            return u;
+        }
+
         public static void Insert(Review r)
         {
             string query = string.Format("INSERT INTO [Review](ReviewRating, ReviewText, ProductId, UserId) VALUES({0}, '{1}', {2}, {3})", r.ReviewRating, r.ReviewText, r.ProductId, r.UserId);
             DatabaseConnection.ExecuteNonQuery(query);
         }
-        /**/
-        public static void Update(int ProductId, string productName, decimal productPrice)
+        
+        public static void Update(int ReviewId, int reviewRating, string reviewText)
         {
-            string query = "UPDATE [Product] SET ProductName = '" + productName + "', ProductPrice = " + productPrice + " WHERE ProductId = " + ProductId;
+            string query = "UPDATE [Review] SET ReviewRating = '" + reviewRating + "', ReviewText = '" + reviewText + "' WHERE ReviewId = " + ReviewId;
             DatabaseConnection.ExecuteNonQuery(query);
         }
-        public static void Update(int ProductId, string productName, string productImage, decimal productPrice)
+
+        //public static void Update(int ProductId, string productName, string productImage, decimal productPrice)
+        //{
+        //    string query = "UPDATE [Product] SET ProductName = '" + productName + "', ProductImage = '" + productImage + "', ProductPrice = " + productPrice + " WHERE ProductId = " + ProductId;
+        //    DatabaseConnection.ExecuteNonQuery(query);
+        //}
+        
+        public static void Delete(int ReviewId)
         {
-            string query = "UPDATE [Product] SET ProductName = '" + productName + "', ProductImage = '" + productImage + "', ProductPrice = " + productPrice + " WHERE ProductId = " + ProductId;
-            DatabaseConnection.ExecuteNonQuery(query);
-        }
-        /**/
-        public static void Delete(int ProductId)
-        {
-            string query = "DELETE [Review] WHERE ProductId = " + ProductId;
+            string query = "DELETE [Review] WHERE ReviewId = " + ReviewId;
             DatabaseConnection.ExecuteNonQuery(query);
         }
     }
